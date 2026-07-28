a854005bab0d2c1e29ceeff0b464f7b4101d4262
ansh307 <anshsoni55333@gmail.com>
2026-07-28 17:11:06 +0530

    updated workflow: No expose backend on 30000

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,13 +53,13 @@ resource "aws_security_group" "ec2_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    description = "Backend port"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # ingress {
+  #   description = "Backend port"
+  #   from_port   = 3000
+  #   to_port     = 3000
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
 
   egress {
     from_port   = 0
