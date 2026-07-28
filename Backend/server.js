88a656b26a082005cfe4401b334cab3c54bfca82
ansh307 <anshsoni55333@gmail.com>
2026-07-28 16:19:13 +0530

    updated server.js for EC2 deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,12 @@
-require("dotenv").config()
-const app = require('./src/app');
-const connectDB = require("./src/db/db")
+require("dotenv").config();
+const app = require("./src/app");
+const connectDB = require("./src/db/db");
 
 connectDB();
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log('Server is running locally on port ' + PORT);
-    });
-}
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is running locally on port " + PORT);
+});
 
 module.exports = app;
